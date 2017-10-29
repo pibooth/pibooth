d180215e03de23df7a0c59ae12d1e1c3336e18e8
anxuae <anxuae-prog@yahoo.fr>
2017-10-29 16:15:17 +0100

    Avoid too many window update if no event happen

--- pibooth/window.py
+++ pibooth/window.py
@@ -11,22 +11,31 @@ class PtbWindow(object):
 
     def __init__(self, size):
         self.width, self.height = size
+        pygame.display.set_caption('Pibooth')
         self.surface = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE)
         self.is_fullscreen = False
 
         self._counter = 0
+        self._current_frame = None
+
+    def _show_and_memorize(self, image_name):
+        """Show image and memorize an image. If the image is the same
+        as the current displayed, nothing is done.
+        """
+        if self._current_frame != image_name:
+            self.surface.blit(pictures.get_image(image_name), (0, 0))
+            pygame.display.update()
+            self._current_frame = image_name
 
     def show_intro(self):
         """Show introduction view.
         """
-        self.surface.blit(pictures.get_image("intro.png"), (0, 0))
-        pygame.display.update()
+        self._show_and_memorize("intro.png")
 
     def show_instructions(self):
         """Show instructions view.
         """
-        self.surface.blit(pictures.get_image("instructions.png"), (0, 0))
-        pygame.display.update()
+        self._show_and_memorize("instructions.png")
 
     def show_counter(self, restart=False):
         """Show the next counter view.
@@ -34,20 +43,17 @@ class PtbWindow(object):
         if restart or self._counter > 4:  # Max 4 view
             self._counter = 0
         self._counter += 1
-        self.surface.blit(pictures.get_image("pose{}.png".format(self._counter)), (0, 0))
-        pygame.display.update()
+        self._show_and_memorize("pose{}.png".format(self._counter))
 
     def show_wait(self):
         """Show wait view.
         """
-        self.surface.blit(pictures.get_image("processing.png"), (0, 0))
-        pygame.display.update()
+        self._show_and_memorize("processing.png")
 
     def show_finished(self):
         """Show finished view.
         """
-        self.surface.blit(pictures.get_image("finished.png"), (0, 0))
-        pygame.display.update()
+        self._show_and_memorize("finished.png")
 
     def show_image(self, pil_image):
         """Show a PIL image.
@@ -57,6 +63,7 @@ class PtbWindow(object):
         data = pil_image.tobytes()
         self.surface.blit(pygame.image.fromstring(data, size, mode), (0, 0))
         pygame.display.update()
+        self._current_frame = None
 
     def show_image_from_file(self, image_file):
         """
@@ -64,12 +71,14 @@ class PtbWindow(object):
         """
         self.surface.blit(pictures.get_image(image_file), (0, 0))
         pygame.display.update()
+        self._current_frame = None
 
     def clear(self):
         """Clear the window content.
         """
         self.surface.fill((0, 0, 0))
         pygame.display.update()
+        self._current_frame = None
 
     def toggle_fullscreen(self):
         """Set window to full screen.
