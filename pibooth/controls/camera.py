c88303d1b047d65299644b456dc0594f42de1db3
anxuae <anxuae-prog@yahoo.fr>
2018-03-06 23:03:27 +0100

    Manage resolution for gphoto preview and capture

--- pibooth/controls/camera.py
+++ pibooth/controls/camera.py
@@ -4,6 +4,7 @@
 import io
 import time
 import subprocess
+import pygame
 import picamera
 try:
     import gphoto2 as gp
@@ -11,7 +12,7 @@ except ImportError:
     gp = None  # gphoto2 is optional
 from PIL import Image, ImageFont, ImageDraw
 from pibooth import pictures, fonts
-from pibooth.utils import LOGGER, PoolingTimer
+from pibooth.utils import LOGGER, PoolingTimer, timeit
 
 
 def rpi_camera_connected():
@@ -48,12 +49,43 @@ def gp_camera_connected():
     return False
 
 
-class RpiCamera(object):
+class BaseCamera(object):
+
+    def __init__(self, resolution):
+        self._cam = None
+        self._border = 50
+        self._window = None
+        self.resolution = resolution
+
+    def get_rect(self):
+        """Return a Rect object (as defined in pygame) for resizing preview and images
+        in order to fit to the defined window.
+        """
+        rect = self._window.get_rect()
+        res = pictures.resize_keep_aspect_ratio(self.resolution,
+                                                (rect.width - 2 * self._border, rect.height - 2 * self._border))
+        return pygame.Rect(rect.centerx - res[0] // 2, rect.centery - res[1] // 2, res[0], res[1])
+
+    def get_overlay(self, size, text, alpha):
+        """Return a PIL image with the given text that can be used
+        as an overlay for the camera.
+        """
+        font = ImageFont.truetype(fonts.get_filename("Amatic-Bold.ttf"), size[1] * 8 // 10)
+        image = Image.new('RGBA', size)
+        draw = ImageDraw.Draw(image)
+        txt_width, txt_height = draw.textsize(text, font=font)
+        position = ((size[0] - txt_width) // 2, (size[1] - txt_height) // 2 - size[1] // 10)
+        draw.text(position, text, (255, 255, 255, alpha), font=font)
+        return image
+
+
+class RpiCamera(BaseCamera):
 
     """Camera management
     """
 
     def __init__(self, iso=200, resolution=(1920, 1080), rotation=0):
+        BaseCamera.__init__(self, resolution)
         self._cam = picamera.PiCamera()
         self._cam.framerate = 15  # Slower is necessary for high-resolution
         self._cam.video_stabilization = True
@@ -62,18 +94,13 @@ class RpiCamera(object):
         self._cam.iso = iso
         self._cam.rotation = rotation
 
-        self._border = 50
-        self._preview_window = None
-
     def preview(self, window, flip=True):
         """Display a preview on the given Rect (flip if necessary).
         """
-        rect = window.get_rect()
-        res = pictures.resize_keep_aspect_ratio(self._cam.resolution,
-                                                (rect.width - 2 * self._border, rect.height - 2 * self._border))
-        # (x, y, width, height)
-        self._preview_window = (rect.centerx - res[0] // 2, rect.centery - res[1] // 2, res[0], res[1])
-        self._cam.start_preview(resolution=res, hflip=flip, fullscreen=False, window=self._preview_window)
+        self._window = window
+        rect = self.get_rect()
+        self._cam.start_preview(resolution=(rect.width, rect.height), hflip=flip,
+                                fullscreen=False, window=tuple(rect))
 
     def preview_countdown(self, timeout, alpha=60):
         """Show a countdown of `timeout` seconds on the preview.
@@ -86,20 +113,13 @@ class RpiCamera(object):
             raise EnvironmentError("Preview shall be started first")
 
         # Create an image padded to the required size
-        size = (((self._preview_window[2] + 31) // 32) * 32,
-                ((self._preview_window[3] + 15) // 16) * 16)
-
-        font = ImageFont.truetype(fonts.get_filename("Amatic-Bold.ttf"), size[1] * 8 // 10)
+        rect = self.get_rect()
+        size = (((rect.width + 31) // 32) * 32, ((rect.height + 15) // 16) * 16)
 
         while timeout > 0:
-            txt = str(timeout)
-            image = Image.new('RGBA', size)
-            draw = ImageDraw.Draw(image)
-            txt_width, txt_height = draw.textsize(txt, font=font)
-            position = ((size[0] - txt_width) // 2, (size[1] - txt_height) // 2 - size[1] // 10)
-            draw.text(position, txt, (255, 255, 255, alpha), font=font)
+            image = self.get_overlay(size, str(timeout), alpha)
             overlay = self._cam.add_overlay(image.tobytes(), image.size, layer=3,
-                                            window=self._preview_window, fullscreen=False)
+                                            window=tuple(rect), fullscreen=False)
             time.sleep(1)
             timeout -= 1
             self._cam.remove_overlay(overlay)
@@ -113,7 +133,7 @@ class RpiCamera(object):
         """Stop the preview.
         """
         self._cam.stop_preview()
-        self._preview_window = None
+        self._window = None
 
     def capture(self, filename=None, pil_format='png'):
         """Capture a picture in a file. If no filename given a PIL image
@@ -136,55 +156,61 @@ class RpiCamera(object):
         self._cam.close()
 
 
-class GpCamera(object):
+class GpCamera(BaseCamera):
 
     """Gphoto2 camera management.
     """
 
     def __init__(self, iso=200, resolution=(1920, 1080), rotation=0):
+        BaseCamera.__init__(self, resolution)
         gp.check_result(gp.use_python_logging())
         self._cam = gp.Camera()
         self._cam.init()
         self._config = self._cam.get_config()
-        self._resolution = resolution
 
-        self._window = None  # Window where the preview is displayed
         self._vflip = False
-        self._border = 50
-
+        self._rotation = rotation
         self._set_config_value('imgsettings', 'iso', iso)
+        self._cam.set_config(self._config)
 
     def _set_config_value(self, section, option, value):
-        for child in self._config.get_children():
-            if child.get_name() == section:
-                for subchild in child.get_children():
-                    if subchild.get_name() == option:
-                        LOGGER.debug('Set %s (%s/%s) to %s', subchild.get_label(),
-                                     child.get_name(), subchild.get_name(), value)
-                        subchild.set_value(str(value))
-                        return
-        raise ValueError('Unsupported setting {}/{}'.format(section, option))
+        """Set camera configuration. This method dont send the updated
+        configuration to the camera (avoid connection flooding if several
+        values to be changed)
+        """
+        try:
+            LOGGER.debug('Setting option %s/%s=%s', section, option, value)
+            child = self._config.get_child_by_name(section).get_child_by_name(option)
+            choices = [c for c in child.get_choices()]
+            if not choices or value in choices:
+                child.set_value(str(value))
+            else:
+                LOGGER.warning("Invalid value '%s' for option %s (possible choices: %s)", value, option, choices)
+        except gp.GPhoto2Error:
+            raise ValueError('Unsupported setting {}/{}={}'.format(section, option, value))
 
     def _get_preview_image(self):
-        camera_file = self._cam.capture_preview()
-        file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
-        image = Image.open(io.BytesIO(memoryview(file_data)))
-        if self._vflip:
-            image = image.transpose(Image.FLIP_LEFT_RIGHT)
-
-        return image.resize(self.get_rect())
-
-    def get_rect(self):
-        """Return the available surface to display image.
+        """Capture a new preview image.
         """
-        rect = self._window.get_rect()
-        return pictures.resize_keep_aspect_ratio(self._resolution,
-                                                 (rect.width - 2 * self._border, rect.height - 2 * self._border))
+        with timeit('Capturing new preview image'):
+            camera_file = self._cam.capture_preview()
+            file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
+            image = Image.open(io.BytesIO(memoryview(file_data)))
+            if self._vflip:
+                image = image.transpose(Image.FLIP_LEFT_RIGHT)
+
+            image = image.resize(pictures.resize_keep_aspect_ratio(image.size, self.resolution, 'outer'))
+            image = image.crop(pictures.crop_to_size(image.size, self.resolution))
+
+        # Resize to the window rect (outer because rect already resized innner, see 'get_rect')
+        rect = self.get_rect()
+        return image.resize(pictures.resize_keep_aspect_ratio(image.size,  (rect.width, rect.height), 'outer'))
 
     def preview(self, window, flip=True):
+        """Setup the preview.
+        """
         self._window = window
         self._vflip = flip
-        LOGGER.debug('Capturing new preview image')
         self._window.show_image(self._get_preview_image())
 
     def preview_countdown(self, timeout, alpha=60):
@@ -195,18 +221,15 @@ class GpCamera(object):
         if timeout < 1:
             raise ValueError("Start time shall be greater than 0")
 
-        size = self.get_rect()
-        font = ImageFont.truetype(fonts.get_filename("Amatic-Bold.ttf"), size[1] * 8 // 10)
-
+        overlay = None
         timer = PoolingTimer(timeout)
         while not timer.is_timeout():
-            txt = str(int(timer.remaining() + 1))
             image = self._get_preview_image()
-            overlay = Image.new('RGBA', image.size)
-            draw = ImageDraw.Draw(overlay)
-            txt_width, txt_height = draw.textsize(txt, font=font)
-            position = ((size[0] - txt_width) // 2, (size[1] - txt_height) // 2 - size[1] // 10)
-            draw.text(position, txt, (255, 255, 255, alpha), font=font)
+            remaining = int(timer.remaining() + 1)
+            if not overlay or remaining != timeout:
+                # Rebluid overlay only if remaining number has changed
+                overlay = self.get_overlay(image.size, str(remaining), alpha)
+                timeout = remaining
             image.paste(overlay, (0, 0), overlay)
             self._window.show_image(image)
 
@@ -233,11 +256,17 @@ class GpCamera(object):
         camera_file = self._cam.capture_preview()
         file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
         image = Image.open(io.BytesIO(memoryview(file_data)))
+        image = image.resize(pictures.resize_keep_aspect_ratio(image.size, self.resolution, 'outer'), Image.ANTIALIAS)
+        image = image.crop(pictures.crop_to_size(image.size, self.resolution))
+
+        # Resize to the window rect (outer because rect already resized innner, see 'get_rect')
+        rect = self.get_rect()
+        size = pictures.resize_keep_aspect_ratio(image.size,  (rect.width, rect.height), 'outer')
 
         if self._vflip:
-            self._window.show_image(image.transpose(Image.FLIP_LEFT_RIGHT).resize(self.get_rect()))
+            self._window.show_image(image.transpose(Image.FLIP_LEFT_RIGHT).resize(size))
         else:
-            self._window.show_image(image.resize(self.get_rect()))
+            self._window.show_image(image.resize(size))
         if filename:
             image.save(filename)
             return filename
--- pibooth/pictures/__init__.py
+++ pibooth/pictures/__init__.py
@@ -12,37 +12,66 @@ def get_filename(name):
     return osp.join(osp.dirname(osp.abspath(__file__)), PtbConfigParser.language, name)
 
 
-def resize_keep_aspect_ratio(original_size, target_size):
-    """Return a new size included in the target one and
-    keeping the original image's aspect ratio.
+def resize_keep_aspect_ratio(original_size, target_size, resize_type='inner'):
+    """Return a new size included (if 'inner') or excluded (if 'outer') in the
+    targeted one by resizing and keeping the original image's aspect ratio.
     """
+    # Get current and desired ratio for the images
+    img_ratio = original_size[0] / float(original_size[1])
+    ratio = target_size[0] / float(target_size[1])
+
     ix, iy = original_size
-    if ix > iy:
+    if ratio > img_ratio:
         # fit to width
         scale_factor = target_size[0] / float(ix)
         sy = scale_factor * iy
-        if sy > target_size[1]:
+        if sy > target_size[1] and resize_type == 'inner':
             scale_factor = target_size[1] / float(iy)
             sx = scale_factor * ix
             sy = target_size[1]
         else:
             sx = target_size[0]
-    else:
+    elif ratio < img_ratio:
         # fit to height
         scale_factor = target_size[1] / float(iy)
         sx = scale_factor * ix
-        if sx > target_size[0]:
+        if sx > target_size[0] and resize_type == 'inner':
             scale_factor = target_size[0] / float(ix)
             sx = target_size[0]
             sy = scale_factor * iy
         else:
             sy = target_size[1]
+    else:
+        return target_size
     return (int(sx), int(sy))
+
+
+def crop_to_size(original_size, target_size, crop_type='middle'):
+    """Return a new size included in the target one by croping and
+    keeping the original image's aspect ratio.
+    """
+    if crop_type.endswith('left'):
+        x = 0
+    elif crop_type.endswith('middle'):
+        x = (original_size[0] - target_size[0]) // 2
+    elif crop_type.endswith('right'):
+        x = original_size[0] - target_size[0]
+
+    if crop_type.startswith('top'):
+        y = 0
+    elif crop_type.startswith('middle'):
+        y = (original_size[1] - target_size[1]) // 2
+    elif crop_type.startswith('bottom'):
+        y = original_size[1] - target_size[1]
+
+    return (x, y, target_size[0] + x, target_size[1] + y)
 
 
 def get_image(name, size=None, antialiasing=True):
     """Return a Pygame image. If a size is given, the image is
     resized keeping the original image's aspect ratio.
+
+    :param antialiasing: use antialiasing algorithm when resize
     """
     if not size:
         return pygame.image.load(get_filename(name)).convert()
