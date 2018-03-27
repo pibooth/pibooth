abdf9c914050161e9f7abd9f0798ba3db7767f04
Tony Anxuae <anxuae-prog@yahoo.fr>
2018-03-27 16:01:47 +0200

    Improve readability of the timeit function logs

--- pibooth/utils.py
+++ pibooth/utils.py
@@ -1,20 +1,69 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import time
 import logging
 import contextlib
 
+
 LOGGER = logging.getLogger("pibooth")
+
+
+class BlockConsoleHandler(logging.StreamHandler):
+
+    pattern_indent = '+< '
+    pattern_blocks = '|  '
+    pattern_dedent = '+> '
+    current_indent = ''
+
+    def emit(self, record):
+        cls = self.__class__
+        record.msg = '{}{}'.format(cls.current_indent, record.msg)
+        if cls.current_indent.endswith(cls.pattern_indent):
+            cls.current_indent = (cls.current_indent[:-len(cls.pattern_indent)] + cls.pattern_blocks)
+        elif cls.current_indent.endswith(cls.pattern_dedent):
+            cls.current_indent = cls.current_indent[:-len(cls.pattern_dedent)]
+        logging.StreamHandler.emit(self, record)
+
+    @classmethod
+    def indent(cls):
+        """Begin a new log block (works only if level is DEBUG).
+        """
+        if logging.getLogger().getEffectiveLevel() < logging.INFO:
+            cls.current_indent += cls.pattern_indent
+
+    @classmethod
+    def dedent(cls):
+        """End the current log block (works only if level is DEBUG).
+        """
+        if logging.getLogger().getEffectiveLevel() < logging.INFO:
+            cls.current_indent = (cls.current_indent[:-len(cls.pattern_blocks)] + cls.pattern_dedent)
+
+
+def configure_logging(level=logging.INFO, msgfmt=logging.BASIC_FORMAT, datefmt=None):
+    """Configure root logger for console printing.
+    """
+    root = logging.getLogger()
+    if not root.handlers:
+        hdlr = BlockConsoleHandler(sys.stdout)
+        hdlr.setFormatter(logging.Formatter(msgfmt, datefmt))
+        root.addHandler(hdlr)
+        if level is not None:
+            root.setLevel(level)
 
 
 @contextlib.contextmanager
 def timeit(description):
+    """Measure time execution.
+    """
+    BlockConsoleHandler.indent()
     LOGGER.info(description)
     start = time.time()
     try:
         yield
     finally:
-        LOGGER.debug("    -> took %0.3f seconds.", time.time() - start)
+        BlockConsoleHandler.dedent()
+        LOGGER.debug("took %0.3f seconds", time.time() - start)
 
 
 class PoolingTimer(object):
