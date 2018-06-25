89f28eedc7f2f5b93320dc7ba49fb72a638f96b1
werdeil <werdeil@hotmail.com>
2018-06-25 23:26:06 +0200

    Better handling of the subprocesses in gPhoto camera

--- pibooth/controls/camera.py
+++ pibooth/controls/camera.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import io
+import os
 import time
+import signal
 import subprocess
 import pygame
 import picamera
@@ -206,7 +208,8 @@ class GpCamera(BaseCamera):
         self._capture_hflip = flip
         self._rotation = rotation
         self._iso = str(iso)
-        self.preview_pid = None
+        self.gphoto2_process = None
+        self.omxplayer_process = None
 
     def camera_init(self):
         """
@@ -255,30 +258,32 @@ class GpCamera(BaseCamera):
     def preview(self, window, flip=True, timeout=5):
         """Setup the preview.
         """
-        if not self.preview_pid:
+        if not self.gphoto2_process:
 
             self._window = window
             rect = self.get_rect()
             self._preview_hflip = flip
             # subprocess.call("mkfifo fifo.mjpg", shell=True)
-            subprocess.call("gphoto2 --capture-movie={0}s --stdout> fifo.mjpg &".format(timeout), shell=True)
+            self.gphoto2_process = subprocess.Popen("gphoto2 --capture-movie --stdout> fifo.mjpg &",
+                                                    shell=True,
+                                                    preexec_fn=os.setsid)
             window_rect = '{0},{1},{2},{3}'.format(tuple(rect)[0], tuple(rect)[1], tuple(rect)[0]+tuple(rect)[2], tuple(rect)[1]+tuple(rect)[3])
             command = "omxplayer fifo.mjpg --live --crop 252,0,804,704 --win {0}".format(window_rect)
-            self.preview_pid = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+            self.omxplayer_process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
 
 
     def preview_countdown(self, timeout, alpha=60):
         """Show a countdown of `timeout` seconds on the preview.
         Returns when the countdown is finished.
         """
-        timeout = 5 #int(timeout)
+        timeout = int(timeout)
         if timeout < 1:
             raise ValueError("Start time shall be greater than 0")
 
         overlay = None
         timer = PoolingTimer(timeout)
         while not timer.is_timeout():
-            self.preview(self._window, timeout=timeout)
+            self.preview(self._window)
             remaining = int(timer.remaining() + 1)
             if not overlay or remaining != timeout:
                 # Rebluid overlay only if remaining number has changed
@@ -302,12 +307,18 @@ class GpCamera(BaseCamera):
     def stop_preview(self):
         """Stop the preview.
         """
+        if self.gphoto2_process:
+            os.killpg(os.getpgid(self.gphoto2_process.pid), signal.SIGTERM)
+            self.gphoto2_process = None
+        if self.omxplayer_process:
+            os.killpg(os.getpgid(self.omxplayer_process.pid), signal.SIGTERM)
+            self.omxplayer_process = None
         self._window = None
+
 
     def capture(self, filename):
         """Capture a picture in a file.
         """
-        self.preview_pid = 0
         self.camera_init()
         self._captures[filename] = self._cam.capture(gp.GP_CAPTURE_IMAGE)
         time.sleep(1)  # Necessary to let the time for the camera to save the image
