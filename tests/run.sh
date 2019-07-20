53f695173c8ed4e1b9061126f331a475a3e84f27
anxuae <anxuae-prog@yahoo.fr>
2019-07-20 11:51:40 +0200

    Move mocks tests in a dedicated folder

--- tests/run.sh
+++ tests/run.sh
@@ -2,7 +2,7 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-export PATH=$SCRIPTPATH:$PATH
-export PYTHONPATH=$SCRIPTPATH/..:$SCRIPTPATH:$PYTHONPATH
+export PATH=$SCRIPTPATH/mocks:$PATH
+export PYTHONPATH=$SCRIPTPATH/..:$SCRIPTPATH/mocks:$PYTHONPATH
 
 python3 $SCRIPTPATH/../pibooth/booth.py $@
