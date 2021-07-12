b863e8f1f22683f203c9c6046a6f90f539077756
anxuae <anxuae-prog@yahoo.fr>
2021-07-12 18:43:21 +0200

    Add favicon for doc

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ html_logo = 'images/pibooth.png'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'images/icon.png'
+html_favicon = 'images/favicon.ico'
 
 # If false, no index is generated.
 html_use_index = True
