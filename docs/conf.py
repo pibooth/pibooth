51f7773257a4f8ea58685851cf5564deec6d8bb4
anxuae <anxuae-prog@yahoo.fr>
2021-07-12 19:11:09 +0200

    Try to fix logo display on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,9 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'logo_only': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -58,7 +61,7 @@ html_static_path = ['_static']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'images/pibooth.png'
+html_logo = 'https://raw.githubusercontent.com/pibooth/pibooth/master/docs/images/pibooth.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
