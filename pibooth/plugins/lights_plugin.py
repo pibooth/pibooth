48d2fd37772e5f6a1db508e172b27c62b5343e67
werdeil <werdeil@hotmail.com>
2023-03-19 22:54:06 +0100

    add try catch to handle case where the usb is not plugged

--- pibooth/plugins/lights_plugin.py
+++ pibooth/plugins/lights_plugin.py
@@ -20,24 +20,36 @@ class LightsPlugin(object):
         if app.previous_picture_file and app.printer.is_ready()\
                 and app.count.remaining_duplicates > 0:
             app.leds.blink(on_time=self.blink_time, off_time=self.blink_time)
-            app.mcp2221_picture_led.value = True
-            app.mcp2221_print_led.value = True
+            try:
+                app.mcp2221_picture_led.value = True
+                app.mcp2221_print_led.value = True
+            except AttributeError:
+                pass
+
         else:
             app.leds.capture.blink(on_time=self.blink_time, off_time=self.blink_time)
             app.leds.printer.off()
-            app.mcp2221_picture_led.value = True
-            app.mcp2221_print_led.value = False
-            
+            try:
+                app.mcp2221_picture_led.value = True
+                app.mcp2221_print_led.value = False
+            except AttributeError:
+                pass
 
     @pibooth.hookimpl
     def state_wait_do(self, app, events):
         if app.find_print_event(events) and app.previous_picture_file and app.printer.is_ready():
             if app.count.remaining_duplicates <= 0:
                 app.leds.printer.off()
-                app.mcp2221_print_led.value = False
+                try:
+                    app.mcp2221_print_led.value = False
+                except AttributeError:
+                    pass
             else:
                 app.leds.printer.on()
-                app.mcp2221_print_led.value = True
+                try:
+                    app.mcp2221_print_led.value = True
+                except AttributeError:
+                    pass
                 time.sleep(1)  # Just to let the LED switched on
                 app.leds.blink(on_time=self.blink_time, off_time=self.blink_time)
 
@@ -47,50 +59,74 @@ class LightsPlugin(object):
     @pibooth.hookimpl
     def state_wait_exit(self, app):
         app.leds.off()
-        app.mcp2221_picture_led.value = False
-        app.mcp2221_print_led.value = False
+        try:
+            app.mcp2221_picture_led.value = False
+            app.mcp2221_print_led.value = False
+        except AttributeError:
+            pass
 
     @pibooth.hookimpl
     def state_choose_enter(self, app):
         app.leds.blink(on_time=self.blink_time, off_time=self.blink_time)
-        app.mcp2221_picture_led.value = True
-        app.mcp2221_print_led.value = True
+        try:
+            app.mcp2221_picture_led.value = True
+            app.mcp2221_print_led.value = True
+        except AttributeError:
+            pass
 
     @pibooth.hookimpl
     def state_choose_exit(self, app):
         if app.capture_nbr == app.capture_choices[0]:
             app.leds.capture.on()
             app.leds.printer.off()
-            app.mcp2221_picture_led.value = True
-            app.mcp2221_print_led.value = False
+            try:
+                app.mcp2221_picture_led.value = True
+                app.mcp2221_print_led.value = False
+            except AttributeError:
+                pass
         elif app.capture_nbr == app.capture_choices[1]:
             app.leds.printer.on()
             app.leds.capture.off()
-            app.mcp2221_picture_led.value = False
-            app.mcp2221_print_led.value = True
+            try:
+                app.mcp2221_picture_led.value = False
+                app.mcp2221_print_led.value = True
+            except AttributeError:
+                pass
 
     @pibooth.hookimpl
     def state_chosen_exit(self, app):
         app.leds.off()
-        app.mcp2221_picture_led.value = False
-        app.mcp2221_print_led.value = False
+        try:
+            app.mcp2221_picture_led.value = False
+            app.mcp2221_print_led.value = False
+        except AttributeError:
+            pass
 
     @pibooth.hookimpl
     def state_print_enter(self, app):
         app.leds.blink(on_time=self.blink_time, off_time=self.blink_time)
-        app.mcp2221_picture_led.value = True
-        app.mcp2221_print_led.value = True
+        try:
+            app.mcp2221_picture_led.value = True
+            app.mcp2221_print_led.value = True
+        except AttributeError:
+            pass
 
     @pibooth.hookimpl
     def state_print_do(self, app, events):
         if app.find_print_event(events):
             app.leds.printer.on()
             app.leds.capture.off()
-            app.mcp2221_picture_led.value = False
-            app.mcp2221_print_led.value = True
+            try:
+                app.mcp2221_picture_led.value = False
+                app.mcp2221_print_led.value = True
+            except AttributeError:
+                pass
 
     @pibooth.hookimpl
     def state_finish_enter(self, app):
         app.leds.off()
-        app.mcp2221_picture_led.value = False
-        app.mcp2221_print_led.value = False
+        try:
+            app.mcp2221_picture_led.value = False
+            app.mcp2221_print_led.value = False
+        except AttributeError:
+            pass
