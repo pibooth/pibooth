605ca710a82b950733b4e66aa606c90d6cf5ea39
anxuae <anxuae-prog@yahoo.fr>
2023-05-03 23:13:51 +0200

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -148,13 +148,13 @@ def pygame_loop(init_pygame):
 def scene_builder():
 
     def create(name):
-        background_sprite = sprites.ImageSprite((0, 0, 0), size=(400, 400))
+        background_sprite = sprites.ImageSprite(None, (0, 0, 0), size=(400, 400), layer=0)
         background_sprite.set_crop()
-        statusbar_sprite = sprites.StatusBarSprite()
+        statusbar_sprite = sprites.StatusBarSprite(None)
 
         scene = get_scene('pygame', name)
-        scene.add_sprite(background_sprite, False, layer=0)
-        scene.add_sprite(statusbar_sprite, False, layer=5)
+        scene.add_sprite(background_sprite)
+        scene.add_sprite(statusbar_sprite)
         scene.set_outlines(True)
         scene.set_text_color((255, 255, 255))
         scene.set_arrows(scene.ARROW_BOTTOM, 0)
