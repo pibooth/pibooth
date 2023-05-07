65f035551e2103c83d61c2da7dbc4492cd29bbe6
anxuae <anxuae-prog@yahoo.fr>
2023-05-07 12:09:57 +0200

    Add libcamera rotation

--- pibooth/camera/libcamera.py
+++ pibooth/camera/libcamera.py
@@ -53,11 +53,11 @@ class LibCamera(BaseCamera):
         """
         self._cam.stop()
         self._preview_config['format'] = 'BGR888'
-        self._preview_config['transform'] = Transform(hflip=self.preview_flip)
+        self._preview_config['transform'] = Transform(rotation=self.preview_rotation, hflip=self.preview_flip)
         self._cam.configure(self._preview_config)
 
         self._capture_config['size'] = self.resolution
-        self._capture_config['transform'] = Transform(hflip=self.capture_flip)
+        self._capture_config['transform'] = Transform(rotation=self.capture_rotation, hflip=self.capture_flip)
         self._cam.configure(self._preview_config)
         self._cam.start()
 
